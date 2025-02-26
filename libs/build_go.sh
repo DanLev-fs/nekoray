4497e7b15294179800eaa870b4271e8c5f4a5a46
DanLev <40503681+DanLev-fs@users.noreply.github.com>
2025-02-26 03:02:13 +0300

    Update build_go.sh

--- libs/build_go.sh
+++ libs/build_go.sh
@@ -17,11 +17,13 @@ export CGO_ENABLED=0
 
 #### Go: updater ####
 pushd go/cmd/updater
+go mod tidy
 [ "$GOOS" == "darwin" ] || go build -o $DEST -trimpath -ldflags "-w -s"
 [ "$GOOS" == "linux" ] && mv $DEST/updater $DEST/launcher || true
 popd
 
 #### Go: nekobox_core ####
 pushd go/cmd/nekobox_core
+go mod tidy
 go build -v -o $DEST -trimpath -ldflags "-w -s -X github.com/matsuridayo/libneko/neko_common.Version_neko=$version_standalone" -tags "with_clash_api,with_gvisor,with_quic,with_wireguard,with_utls,with_ech"
 popd
